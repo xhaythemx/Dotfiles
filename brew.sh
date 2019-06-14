7285d159dfcd21ead993c23edabcbe08bf164365
xhaythemx <xhaythemx@live.com>
2019-06-14 11:40:09 +0200

    Update brew.sh

---
+++ brew.sh
@@ -0,0 +1,9 @@
+brew install zsh
+brew install zsh-syntax-highlighting
+brew install cmatrix
+brew install cowsay
+brew install fortune
+brew install lolcat
+brew install neofetch
+brew install nmap
+brew install tree
